99e60a6e3d5d217aefa5c38bd8a3034c209130b1
mkozhevnikov <mkozhevnikov@servicetitan.com>
2021-10-19 17:56:21 -0700

    implemented repository for MongoDB

---
+++ src/MongoDB/ICollectionContext.cs
@@ -0,0 +1,11 @@
+using MongoDB.Driver;
+
+namespace Boilerplate.MongoDB
+{
+    public interface ICollectionContext
+    {
+        string Name { get; }
+
+        IMongoDatabase DB { get; }
+    }
+}
