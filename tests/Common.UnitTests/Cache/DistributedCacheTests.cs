07f7dd693e218f8d4e6d08f6611cd14ef873f996
mkozhevnikov <mkozhevnikov@servicetitan.com>
2022-04-02 18:12:34 -0700

    add tests for exists helper method of distributed cache

--- tests/Common.UnitTests/Cache/DistributedCacheTests.cs
+++ tests/Common.UnitTests/Cache/DistributedCacheTests.cs
@@ -16,10 +16,7 @@ public class DistributedCacheTests
     public async Task MemoryCache_SetAsJson_DefaultOptions_HitCache()
     {
         var key = "k1";
-        var model = new TestModel {
-            IntProp = 123,
-            DateTimeProp = DateTime.Today
-        };
+        var model = new TestModel();
 
         await cache.SetAsJsonAsync(key, model);
 
@@ -31,10 +28,7 @@ public class DistributedCacheTests
     public async Task MemoryCache_SetAsJson_ExpiresIn_HitCache()
     {
         var key = "k2";
-        var model = new TestModel {
-            IntProp = 123,
-            DateTimeProp = DateTime.Today
-        };
+        var model = new TestModel();
 
         await cache.SetAsJsonAsync(key, model, TimeSpan.FromDays(1));
 
@@ -46,10 +40,7 @@ public class DistributedCacheTests
     public async Task MemoryCache_SetAsJson_SlidingExpiration_HitCache()
     {
         var key = "k3";
-        var model = new TestModel {
-            IntProp = 123,
-            DateTimeProp = DateTime.Today
-        };
+        var model = new TestModel();
 
         await cache.SetAsJsonAsync(key, model, new DistributedCacheEntryOptions {
             SlidingExpiration = TimeSpan.FromDays(1)
@@ -59,15 +50,11 @@ public class DistributedCacheTests
         cachedValue.Should().NotBeNull().And.NotBeEmpty();
     }
 
-
     [Fact]
     public async Task MemoryCache_SetAsJson_WriteIndented_SerializedIndented()
     {
         var key = "k4";
-        var model = new TestModel {
-            IntProp = 123,
-            DateTimeProp = DateTime.Today
-        };
+        var model = new TestModel();
 
         await cache.SetAsJsonAsync(key, model, jsonSerializerOptions: new JsonSerializerOptions {
             WriteIndented = true
@@ -76,6 +63,29 @@ public class DistributedCacheTests
         var cachedValue = await cache.GetAsync(key);
         var jsonSerializedModel = Encoding.UTF8.GetString(cachedValue);
         jsonSerializedModel.Should().NotBeEmpty().And.MatchRegex("^{(\\s+\"\\w+\\\".+\\n)+}$");
+    }
+
+    [Fact]
+    public async Task MemoryCache_Exists_HitCache()
+    {
+        var key = "k5";
+        await cache.SetAsync(key, Encoding.UTF8.GetBytes("test value"));
+
+        var sut = await cache.ExistsAsync(key);
+
+        sut.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task MemoryCache_Exists_Removed_MissCache()
+    {
+        var key = "k6";
+        await cache.SetAsync(key, Encoding.UTF8.GetBytes("test value"));
+        await cache.RemoveAsync(key);
+
+        var sut = await cache.ExistsAsync(key);
+
+        sut.Should().BeFalse();
     }
 
     private readonly IDistributedCache cache;
