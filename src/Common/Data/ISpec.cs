af29ff06cc9ae27231dd0d380b2531a40513df90
mkozhevnikov <mkozhevnikov@servicetitan.com>
2022-04-11 16:42:57 -0700

    add sorted specification to pass sorting options to repository

--- src/Common/Data/ISpec.cs
+++ src/Common/Data/ISpec.cs
@@ -1,15 +1,40 @@
+using System.ComponentModel;
+
 namespace Boilerplate.Common.Data;
 
 using System.Linq.Expressions;
 
 public interface ISpec<T>
 {
+    /// <summary>
+    /// Condition to use with Queryable to filter objects satisfying the spec
+    /// </summary>
     Expression<Func<T, bool>> Expression { get; }
+
+    /// <summary>
+    /// Skipping specification to ignore number of objects
+    /// </summary>
+    int? Skip { get; }
+
+    /// <summary>
+    /// Limiting specification to return certain number of objects
+    /// </summary>
+    int? Take { get; }
+}
+
+public interface ISortedSpec<T> : ISpec<T>
+{
+    /// <summary>
+    /// Sorting specification to return objects in specific order
+    /// </summary>
+    ListSortDescriptionCollection Sort { get; }
 }
 
 public class Spec<T> : ISpec<T>
 {
     public Expression<Func<T, bool>> Expression { get; }
+    public int? Skip { get; set; }
+    public int? Take { get; set; }
 
     public Spec(Expression<Func<T, bool>> expression)
     {
